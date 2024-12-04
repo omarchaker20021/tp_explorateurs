dde9014f6adb2fc9c6502e78651ef5d50dff6632
linab <lina.bnh23@gmail.com>
2024-12-04 16:26:09 +0100

    Commit

---
+++ data/Animal.java
@@ -0,0 +1,22 @@
+package data;
+
+public class Animal extends Object{
+    private int health;
+
+    public Animal(Block block, int health){
+        super(block);
+        this.health = health;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
+    public void setHealth(int health){
+        this.health = health;
+    }
+
+    public void move(Block newblock){
+        setPosition(newblock);
+    }
+}
